9c7a3c18b4a2d1b8c98f173ee549b0b9443bf2c8
stefan521 <stefan521@users.noreply.github.com>
2024-05-05 19:39:38 +0100

    tiny squares inside the piece.

--- tessera/game/Square.swift
+++ tessera/game/Square.swift
@@ -9,16 +9,23 @@ import SpriteKit
 
 class Square: Piece {
 
-    var nodes: Array<SKSpriteNode> = Array()
-    
-    init () {
+    private static var initialPositions: Array<CGPoint> = [
+        CGPoint(x: MAX_X / 2, y: MAX_Y),
+        CGPoint(x: MAX_X / 2 + TILE_SIZE, y: MAX_Y),
+        CGPoint(x: MAX_X / 2, y: MAX_Y + TILE_SIZE),
+        CGPoint(x: MAX_X / 2 + TILE_SIZE, y: MAX_Y + TILE_SIZE)
+    ]
+
+    var nodes: Array<SKSpriteNode> = (0...3).map { index in
         let node = SKSpriteNode()
         node.anchorPoint = CGPoint(x: 0, y: 0)
         node.color = NSColor.black
         node.size = CGSize(width: TILE_SIZE, height: TILE_SIZE)
-        node.position = CGPoint(x: MAX_X / 2, y: MAX_Y)
-        nodes.append(node)
+        node.position = initialPositions[index]
+        return node
     }
+
+    init () {}
 
     func moveLeft() {
         if (nodes.contains { node in node.position.x > MIN_X  }) {
@@ -27,7 +34,7 @@ class Square: Piece {
     }
 
     func moveRight() {
-        if (nodes.contains(where: { node in node.position.x < MAX_X })) {
+        if (nodes.contains { $0.position.x < MAX_X }) {
             nodes.forEach { node in node.position = CGPoint(x: node.position.x + TILE_SIZE, y: node.position.y) }
         }
     }
