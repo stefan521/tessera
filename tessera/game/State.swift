f355aabce9fbd9b01a1fe4568e60d2660c95a537
stefan521 <stefan521@users.noreply.github.com>
2024-05-08 22:49:35 +0100

    fill state

--- tessera/game/State.swift
+++ tessera/game/State.swift
@@ -10,22 +10,39 @@ import SpriteKit
 class State {
     private var positions: Dictionary<String, Optional<SKNode>> = [:]
     
-    private func fromPoint(_ point: CGPoint) -> String {
-        let x = Int(point.x)
-        let y = Int(point.y)
-        return "(\(x),\(y))"
+    init () {
+        buildPositions()
+    }
+
+    private func buildPositions() -> Void {
+        var x = 0.0
+
+        while x <= BOARD_WIDTH {
+            var y = 0.0
+
+            while y <= BOARD_HEIGHT {
+                positions.updateValue(Optional.none, forKey: key(x, y))
+                y += TILE_SIZE
+            }
+
+            x += TILE_SIZE
+        }
+    }
+
+    private func key(_ x: CGFloat, _ y: CGFloat) -> String {
+        return "(\(Int(x)),\(Int(y)))"
     }
 
     func nodeAt(_ point: CGPoint) -> Optional<SKNode> {
-        return positions[fromPoint(point)] ?? Optional.none
+        return positions[key(point.x, point.y)] ?? Optional.none
     }
 
     func clearAt(_ point: CGPoint) -> Void {
-        positions.updateValue(Optional.none, forKey: fromPoint(point))
+        positions.updateValue(Optional.none, forKey: key(point.x, point.y))
     }
 
     func setAt(_ point: CGPoint, _ node: SKNode) -> Void {
-        positions.updateValue(node, forKey: fromPoint(point))
+        positions.updateValue(node, forKey: key(point.x, point.y))
     }
 }
 
